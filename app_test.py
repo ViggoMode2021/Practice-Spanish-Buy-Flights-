4e8ac86f75059227f5269933edfd00a0b5a0472f
Ryan <ryansviglione@gmail.com>
2023-01-10 19:16:41 -0500

    hajiasdg

--- app_test.py
+++ app_test.py
@@ -62,6 +62,29 @@ def test_db_connection():
 
     assert test_result_db == 'bburnerson840@gmail.com'
 
+@pytest.fixture()
+def setup(request):
+    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
+
+    chrome_options = Options()
+    options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+    for option in options:
+        chrome_options.add_argument(option)
+
+    request.cls.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+
+    yield request.cls.driver
+    request.cls.driver.close()
+
 def test_text_present():
     browser = Browser()
     url = 'https://practicespanishbuyflights.com/'
