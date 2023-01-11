7a38bb041b3cabad6140f2fcb7f10ddad7cce7a8
Ryan <ryansviglione@gmail.com>
2023-01-10 21:18:36 -0500

    coaosdf

--- app_test.py
+++ app_test.py
@@ -88,3 +88,46 @@ def test_search_engine_optimization(browser):
     button.click()
     assert browser.is_text_present('Practice Spanish Buy Flights!'), "Not found - let's get on that SEO GAME LIKE A GURU!"
 '''
+
+import pytest
+import pytest_html
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+ 
+#Fixture for Firefox
+@pytest.fixture(scope="class")
+def driver_init(request):
+    ff_driver = webdriver.Firefox()
+    request.cls.driver = ff_driver
+    yield
+    ff_driver.close()
+ 
+#Fixture for Chrome
+@pytest.fixture(scope="class")
+def chrome_driver_init(request):
+    chrome_driver = webdriver.Chrome()
+    request.cls.driver = chrome_driver
+    yield
+    chrome_driver.close()
+ 
+@pytest.mark.usefixtures("driver_init")
+class BasicTest:
+    pass
+class Test_URL(BasicTest):
+        def test_open_url(self):
+            self.driver.get("https://www.lambdatest.com/")
+            print(self.driver.title)
+ 
+            sleep(5)
+ 
+@pytest.mark.usefixtures("chrome_driver_init")
+class Basic_Chrome_Test:
+    pass
+class Test_URL_Chrome(Basic_Chrome_Test):
+        def test_open_url(self):
+            self.driver.get("https://www.lambdatest.com/")
+            print(self.driver.title)
+ 
+            sleep(5)
