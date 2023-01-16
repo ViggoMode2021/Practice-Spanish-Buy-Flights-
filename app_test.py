62bdf59b5a1f87d07282d4db922932741b2fbfbc
Ryan <ryansviglione@gmail.com>
2023-01-16 12:34:27 -0500

    headless

--- app_test.py
+++ app_test.py
@@ -15,6 +15,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument('--no-sandbox')
+options.add_argument('--headless')
 
 #Fixture for Chrome
 @pytest.fixture(scope="class")
