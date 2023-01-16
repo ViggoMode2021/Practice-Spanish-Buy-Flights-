40428dae181f05146f0d90d05ca10fb011a9ba2f
Ryan <ryansviglione@gmail.com>
2023-01-16 09:56:04 -0500

    add pytest

--- app_test.py
+++ app_test.py
@@ -29,7 +29,7 @@ def chrome_driver_init(request):
     yield
     chrome_driver.close()
 
-@pytest.mark.usefixtures("chrome_driver_init")
+'''@pytest.mark.usefixtures("chrome_driver_init")
 class BasicTest:
     pass
 class Test_URL(BasicTest):
@@ -54,7 +54,7 @@ class Test_URL(BasicTest):
             self.driver.find_element(By.ID, ("logout")).click()
             sleep(2)
             find_title = self.driver.find_element(By.TAG_NAME, "h6") 
-            assert find_title.text == "Practice Spanish, Buy Flights!"
+            assert find_title.text == "Practice Spanish, Buy Flights!"'''
 
 #Fixture for Chrome
 @pytest.fixture(scope="class")
@@ -82,6 +82,70 @@ class Test_Levels():
             self.driver.find_element(By.ID, "colombia_verbs").click()
             find_colombia_title = self.driver.find_element(By.TAG_NAME, ('h5'))
             assert find_colombia_title.text == "Verbs - Colombia"
-            sleep(5)
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
+
+        def test_open_argentina_places(self):
+            self.driver.find_element(By.ID, "argentina_places").click()
+            find_argentina_title = self.driver.find_element(By.TAG_NAME, ('h5'))
+            assert find_argentina_title.text == "Places - Argentina"
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
+
+        def test_open_peru_speaking(self):
+            self.driver.find_element(By.ID, "peru_speaking").click()
+            find_peru_title = self.driver.find_element(By.TAG_NAME, ('h5'))
+            assert find_peru_title.text == "Speaking Page - Perú"
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
+
+        def test_open_paraguay_adjectives(self):
+            self.driver.find_element(By.ID, "paraguay_adjectives").click()
+            find_paraguay_title = self.driver.find_element(By.TAG_NAME, ('h5'))
+            assert find_paraguay_title.text == "Adjectives - Paraguay"
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
+
+        def test_open_mexico_foods_and_drinks(self):
+            self.driver.find_element(By.ID, "mexico_foods_and_drinks").click()
+            find_mexico_title = self.driver.find_element(By.TAG_NAME, ('h5'))
+            assert find_mexico_title.text == "Foods + Drinks - México"
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
+
+        def test_open_ecuador_verb_conjugation(self):
+            self.driver.find_element(By.ID, "ecuador_verb_conjugation").click()
+            find_ecuador_title = self.driver.find_element(By.TAG_NAME, ('h1'))
+            assert find_ecuador_title.text == "Verb conjugation practice tense selector"
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
+        
+        def test_open_guatemala_greetings_and_farewells(self):
+            self.driver.find_element(By.ID, "guatemala_greetings_and_farewells").click()
+            find_guatemala_title = self.driver.find_element(By.TAG_NAME, ('h5'))
+            assert find_guatemala_title.text == "Guatemala - Greetings and farewells"
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
+
+        def test_open_honduras_dates(self):
+            self.driver.find_element(By.ID, "honduras_dates").click()
+            find_honduras_title = self.driver.find_element(By.TAG_NAME, ('h5'))
+            assert find_honduras_title.text == "Honduras - Dates"
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
+
+        def test_open_costa_rica_weather(self):
+            self.driver.find_element(By.ID, "costa_rica_weather").click()
+            find_costa_rica_title = self.driver.find_element(By.TAG_NAME, ('h5'))
+            assert find_costa_rica_title.text == "Costa Rica - weather"
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
+
+        def test_open_venezuela_conversation(self):
+            self.driver.find_element(By.ID, "venezuela_conversation").click()
+            find_venezuela_title = self.driver.find_element(By.TAG_NAME, ('h1'))
+            assert find_venezuela_title.text == "Ask a question to Señor Lex"
+            self.driver.find_element(By.ID, "level_selector").click()
+            sleep(3)
 
         
